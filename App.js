753374c8d3725cf4a8da757afd9890e53196d613
Christoph Eck <christoph@milkinteractive.ch>
2019-08-16 07:15:05 +0200

    All screens finished

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { Platform, StatusBar, StyleSheet, View } from 'react-native'
 import { Ionicons, Feather } from '@expo/vector-icons'
 
 import AppNavigator from './navigation/AppNavigator'
-import QuizScreen from './screens/QuizScreen'
+import QuizFinishedScreen from './screens/QuizFinishedScreen'
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false)
@@ -23,7 +23,7 @@ export default function App(props) {
     return (
       <View style={styles.container}>
         {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <QuizScreen />
+        <QuizFinishedScreen />
       </View>
     )
   }
