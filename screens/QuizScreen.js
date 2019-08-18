e5e1f216b4a67d8ba79bdca1e0f36c798b5ea807
Christoph Eck <christoph@milkinteractive.ch>
2019-08-18 10:33:48 +0200

    Quiz: add logic of quiz and ui

--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -8,27 +8,84 @@ import {
     TouchableOpacity,
     View,
 } from 'react-native'
+import { connect } from 'react-redux'
 
 import Colors from '../constants/Colors'
 import QuestionText from '../components/QuestionText'
 import Button from '../components/Button'
+import BigTitle from '../components/BigTitle'
 import Styles from '../constants/Styles'
 
-class Screen extends React.Component {
+class QuizScreen extends React.Component {
+    static navigationOptions = ({ navigation }) => {
+        const { title } = navigation.state.params
+        return {
+            title,
+        }
+    }
+    state = {
+        correct: 0,
+        incorrect: 0,
+        current: 1,
+        showAnswer: false,
+    }
+    onCorrectClicked = () => {
+        this.setState({ ...this.state, correct: this.state.correct+1, current: this.state.current+1 })
+    }
+    onIncorrectClicked = () => {
+        this.setState({ ...this.state, incorrect: this.state.incorrect+1, current: this.state.current+1 })
+    }
+    onFlipCardClicked = () => {
+        this.setState({ ...this.state, showAnswer: !this.state.showAnswer })
+    }
+    restartQuiz = () => {
+        this.setState({
+            correct: 0,
+            incorrect: 0,
+            current: 1,
+            showAnswer: false,
+         })
+    }
     render() {
+        const { deck } = this.props
+        const { questions } = deck
+        const amountOfCards = questions.length
+        const { current, showAnswer, correct } = this.state
+        const currentIndex = (current >= amountOfCards) ? amountOfCards - 1 : current - 1
+        const currentQuestion = questions[currentIndex]
+        const text = showAnswer ? currentQuestion.answer : currentQuestion.question
+        const flipCardButtonText = showAnswer ? 'Show Question' : 'Show Answer'
+        const isLastQuestion = current > amountOfCards
+        if(isLastQuestion) {
+            return (
+                <ScrollView style={Styles.container}>
+                    <BigTitle>Quiz Finished</BigTitle>
+                    <Text style={styles.text}>You had {Math.round(correct/amountOfCards*100)}% correct of all questions.</Text>
+                    <Button onPress={this.restartQuiz}>Restart Quiz</Button>
+                </ScrollView>
+            )
+        }
         return (
             <ScrollView style={Styles.container}>
-                <QuestionText style={styles.text}>Does React Native work with Android?</QuestionText>
-                <Text style={styles.paging}>1 / 2</Text>
-                <Button style={styles.correctButton}>Correct</Button>
-                <Button style={styles.incorrectButton}>Incorrect</Button>
-                <Button>See Answer</Button>
+                <QuestionText style={styles.text}>{text}</QuestionText>
+                <Text style={styles.paging}>{current} / {amountOfCards}</Text>
+                <Button onPress={this.onCorrectClicked} style={styles.correctButton}>Correct</Button>
+                <Button onPress={this.onIncorrectClicked} style={styles.incorrectButton}>Incorrect</Button>
+                <Button onPress={this.onFlipCardClicked}>{flipCardButtonText}</Button>
             </ScrollView>
         )
     }
 }
 
-export default Screen
+function mapStateToProps(decks, { navigation }) {
+    const { title } = navigation.state.params
+    return {
+        title,
+        deck: decks[title]
+    }
+}
+
+export default connect(mapStateToProps)(QuizScreen)
 
 const styles = StyleSheet.create({
     text: {
