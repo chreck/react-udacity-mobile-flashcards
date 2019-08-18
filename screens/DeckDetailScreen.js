6eaa13632eee591dddf4e90ab3184276c624fbcd
Christoph Eck <christoph@milkinteractive.ch>
2019-08-18 09:59:20 +0200

    DeckDetailScreen: delete card and update only when valid deck value is present

--- screens/DeckDetailScreen.js
+++ screens/DeckDetailScreen.js
@@ -10,6 +10,10 @@ import {
     View,
 } from 'react-native'
 import { connect } from 'react-redux'
+import { NavigationActions } from 'react-navigation'
+
+import { delDeck, receive } from '../utils/api'
+import { removeDeck as actionDelDeck } from '../actions'
 
 import Button from '../components/Button'
 import CardText from '../components/CardText'
@@ -17,7 +21,6 @@ import Styles from '../constants/Styles'
 
 class DeckDetailScreen extends React.Component {
     onStartCard = () => {
-        console.log('onStartCard')
         const { title } = this.props
         this.props.navigation.navigate({
             routeName: 'AddCard',
@@ -30,13 +33,18 @@ class DeckDetailScreen extends React.Component {
         console.log('onStartQuiz')
     }
     onDeleteDeck = () => {
-        console.log('onDeleteDeck')
+        const { title, dispatch } = this.props
+        dispatch(actionDelDeck(title))
+        delDeck(title)
+        this.props.navigation.goBack()
+    }
+    shouldComponentUpdate(nextProps) {
+        return nextProps.deck !== null
     }
     render() {
         const { deck } = this.props
         const { title, questions } = deck
         const amountOfCards = questions.length
-        console.log(deck)
         return (
             <ScrollView style={Styles.container}>
                 <CardText text={title} detail={`${amountOfCards} cards`} />
@@ -52,7 +60,7 @@ function mapStateToProps(decks, { navigation }) {
     const { title } = navigation.state.params
     return {
         title,
-        deck: decks[title]
+        deck: decks[title] ? decks[title] : null
     }
 }
 
